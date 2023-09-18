1fe4726533d6619711cc7f111653971073ff8e69
katemikh <cooperation.ca@yahoo.com>
2023-09-18 13:55:50 -0400

    added corrections to lesson-6-2-correction

--- js/index.js
+++ js/index.js
@@ -1,185 +1,208 @@
-const today = new Date();
-const thisYear = today.getFullYear();
+function addCopyrightToFooter() {
+  const today = new Date();
+  const thisYear = today.getFullYear();
 
-const footer = document.querySelector("footer");
+  const footer = document.querySelector("footer");
+  const copyright = document.createElement("p");
+  copyright.innerHTML = `&copy; ${thisYear} Kateryna Mikhailiuk`;
+  footer.appendChild(copyright);
+}
 
-const copyright = document.createElement("p");
-copyright.innerHTML = `&copy; ${thisYear} Kateryna Mikhailiuk`;
-footer.appendChild(copyright);
+function createSkillsList() {
+  const skillsWithIcons = [
+    { skill: "HTML5", icon: "media/img-skills/html.png" },
+    { skill: "CSS3", icon: "media/img-skills/css3.png" },
+    { skill: "JavaScript", icon: "media/img-skills/js.png" },
+    { skill: "Bootstrap", icon: "media/img-skills/bootstrap.png" },
+    { skill: "API", icon: "media/img-skills/api.png" },
+    { skill: "GSAP", icon: "media/img-skills/gsap-bw.png" },
+    { skill: "Figma", icon: "media/img-skills/figma.png" },
+    { skill: "React", icon: "media/img-skills/react.png" },
+    // Add more skills and icons as needed
+  ];
 
+  const boxSkills = document.querySelector(".box__skills");
 
-// Array of skills and their corresponding icons
-const skillsWithIcons = [
-  { skill: "HTML5", icon: "/media/img-skills/html.png" },
-  { skill: "CSS3", icon: "/media/img-skills/css3.png" },
-  { skill: "JavaScript", icon: "/media/img-skills/js.png" },
-  { skill: "Bootstrap", icon: "/media/img-skills/bootstrap.png" },
-  { skill: "API", icon: "/media/img-skills/api.png" },
-  { skill: "GSAP", icon: "/media/img-skills/gsap-bw.png" },
-  { skill: "Figma", icon: "/media/img-skills/figma.png" },
-  { skill: "React", icon: "/media/img-skills/react.png" },
-  // Add more skills and icons as needed
-];
+  // Loop through skills array and create list items with icons
+  skillsWithIcons.forEach((skillData) => {
+    const listItem = document.createElement("li");
+    const skillIcon = document.createElement("img");
 
-const boxSkills = document.querySelector(".box__skills");
+    listItem.textContent = skillData.skill;
+    skillIcon.src = skillData.icon;
+    skillIcon.alt = `${skillData.skill} Icon`;
 
-// Loop through skills array and create list items with icons
-skillsWithIcons.forEach((skillData) => {
-  const listItem = document.createElement("li");
-  const skillIcon = document.createElement("img");
-
-  listItem.textContent = skillData.skill;
-  skillIcon.src = skillData.icon;
-  skillIcon.alt = `${skillData.skill} Icon`;
-
-  listItem.appendChild(skillIcon);
-  boxSkills.appendChild(listItem);
-});
-
-
-// const skills = [
-//   'HTML5',
-//   'CSS3',
-//   'JavaScript',
-//   'Bootstrap',
-//   'API',
-//   'GSAP',
-//   'Figma',
-// ];
-
-// const skillsSection = document.querySelector('#skills');
-// const skillsList = skillsSection.querySelector('ul');
-
-// for (let i = 0; i < skills.length; i++) {
-//   const skill = document.createElement('li');
-//   skill.innerText = skills[i];
-//   skillsList.appendChild(skill);
-// }
-
-
-
-// lesson-4-3
-// DOM Selection
-const messageForm = document.forms.leave_message;
-const messageSection = document.querySelector("#messages");
-const messageList = messageSection.querySelector("ul");
-
-// Event Listener
-messageForm.addEventListener("submit", function (event) {
-  event.preventDefault(); // Prevent default form submission behavior
-
-// Retrieve form field values
-  const usersName = event.target.usersName.value;
-//const usersName = messageForm.usersName.value;
-  const usersEmail = event.target.usersEmail.value;
-//const usersEmail = messageForm.usersEmail.value;
-  const usersMessage = event.target.usersMessage.value;
-//const usersMessage = messageForm.usersMessage.value;
-
-// Log form field values
-  console.log("Name:", usersName);
-  console.log("Email:", usersEmail);
-  console.log("Message:", usersMessage);
-
-//  Clear the form fields
-  event.target.reset();
-//or
-
-  // Clear form fields
-//messageForm.reset();
-
-// Create new list item
-  const newMessage = document.createElement("li");
-  newMessage.innerHTML = `
-  <a href="mailto:${usersEmail}">${usersName} wrote: </a>
-    <span>${usersMessage}</span>
-  `;
-
-// Create remove button
-  const removeButton = document.createElement("button");
-  removeButton.innerText = "Remove";
-  removeButton.type = "button";
-  removeButton.addEventListener("click", function () {
-    const entry = removeButton.parentNode;
-    entry.remove();
-
-// Check if the message list is empty
-    if (messageList.children.length === 0) {
-      messageSection.style.display = "none";
-    }
+    listItem.appendChild(skillIcon);
+    boxSkills.appendChild(listItem);
   });
+}
 
-// Create edit button
-  const editButton = document.createElement("button");
-  editButton.innerText = "Edit";
-  editButton.type = "button";
-  editButton.addEventListener("click", function () {
-    const entry = editButton.parentNode;
-    const span = entry.querySelector("span");
-    const editedMessage = prompt("Edit the message:", span.textContent);
-    if (editedMessage !== null) {
-      span.textContent = editedMessage;
-    }
+function setupLeaveMessageForm() {
+  // DOM Selection
+  const messageForm = document.forms.leave_message;
+  const messageSection = document.querySelector("#messages");
+  const messageList = messageSection.querySelector("ul");
+
+  // Event Listener
+  messageForm.addEventListener("submit", function (event) {
+    event.preventDefault(); // Prevent default form submission behavior
+
+    // Retrieve form field values
+    const usersName = event.target.usersName.value;
+    const usersEmail = event.target.usersEmail.value;
+    const usersMessage = event.target.usersMessage.value;
+
+    // Log form field values
+    console.log("Name:", usersName);
+    console.log("Email:", usersEmail);
+    console.log("Message:", usersMessage);
+
+    //  Clear the form fields
+    event.target.reset();
+
+    //or
+    //messageForm.reset();
+
+    // Create new list item
+    const newMessage = document.createElement("li");
+    newMessage.innerHTML = `
+      <a href="mailto:${usersEmail}">${usersName} wrote: </a>
+      <span>${usersMessage}</span>
+    `;
+    // Create remove button
+    const removeButton = document.createElement("button");
+    removeButton.innerText = "Remove";
+    removeButton.type = "button";
+    removeButton.addEventListener("click", function () {
+      const entry = removeButton.parentNode;
+      entry.remove();
+
+      // Check if the message list is empty
+      if (messageList.children.length === 0) {
+        messageSection.style.display = "none";
+      }
+    });
+
+    // Create edit button
+    const editButton = document.createElement("button");
+    editButton.innerText = "Edit";
+    editButton.type = "button";
+    editButton.addEventListener("click", function () {
+      const entry = editButton.parentNode;
+      const span = entry.querySelector("span");
+      const editedMessage = prompt("Edit the message:", span.textContent);
+      if (editedMessage !== null) {
+        span.textContent = editedMessage;
+      }
+    });
+
+    // Append edit button, remove button, and new message to list
+    newMessage.appendChild(editButton);
+    newMessage.appendChild(removeButton);
+    messageList.appendChild(newMessage);
+
+    // Show the message section
+    messageSection.style.display = "flex";
   });
-
-// Append edit button, remove button, and new message to list
-  newMessage.appendChild(editButton);
-  newMessage.appendChild(removeButton);
-  messageList.appendChild(newMessage);
-
-// Show the message section
-  messageSection.style.display = "flex";
-});
+}
 
 //Projects
 
-// Create a new XMLHttpRequest object
-const githubRequest = new XMLHttpRequest();
+function fetchAndDisplayProjects() {
+  // Create a Fetch API GET request to the GitHub API URL
+  fetch("https://api.github.com/users/katemikh/repos")
+    // Chain a "then" method to handle the response and parse it as JSON
+    .then((response) => response.json())
+    // Chain another "then" method to handle the parsed JSON data
+    .then((repositories) => {
+      // Log the list of repositories in the console
+      console.log(repositories);
 
-// Open a GET request to the GitHub API URL with your GitHub username
-githubRequest.open("GET", "https://api.github.com/users/katemikh/repos");
+      const projectSection = document.getElementById("projects");
+      const projectList = projectSection.querySelector("ul");
 
-// Send the GET request
-githubRequest.send();
+      // Specifying the repository names to display
+      const targetRepositoryNames = [
+        "js-lets-split-our-bill",
+        "js-embraer-presentation-countdown",
+        "Methed-Warm-Floor",
+        "js-travel-destination-search",
+      ];
 
-// Add a "load" event listener to handle the response
-githubRequest.addEventListener("load", function (event) {
-  // Parse the response and store it in the 'repositories' variable
-  const repositories = JSON.parse(this.response);
-  // Log the list of repositories in the console
-  console.log(repositories);
+      for (let i = 0; i < repositories.length; i++) {
+        const repositoryName = repositories[i].name;
 
-  const projectSection = document.getElementById("projects");
-  const projectList = projectSection.querySelector("ul");
+        // Check if the current repository name is in the target list
+        if (targetRepositoryNames.includes(repositoryName)) {
+          const project = document.createElement("li");
+          const repoLink = document.createElement("a");
+          repoLink.href = repositories[i].html_url;
+          repoLink.textContent = repositoryName;
 
-  // Specify the repository names you want to display
-  const targetRepositoryNames = [
-    "js-lets-split-our-bill",
-    "js-embraer-presentation-countdown",
-    "Methed-Warm-Floor",
-    "js-travel-destination-search",
-  ];
+          const description = document.createElement("p");
+          description.textContent = repositories[i].description;
 
-  for (let i = 0; i < repositories.length; i++) {
-    const repositoryName = repositories[i].name;
+          const language = document.createElement("span");
+          language.textContent = "Language: " + repositories[i].language;
 
-    // Check if the current repository name is in the target list
-    if (targetRepositoryNames.includes(repositoryName)) {
-      const project = document.createElement("li");
-      const repoLink = document.createElement("a");
-      repoLink.href = repositories[i].html_url;
-      repoLink.textContent = repositoryName;
+          project.appendChild(repoLink);
+          project.appendChild(description);
+          project.appendChild(language);
+          projectList.appendChild(project);
+        }
+      }
+    })
+    // Chain a "catch" method to handle errors (optional)
+    .catch((error) => {
+      console.error("Error fetching data: ", error);
+    });
+}
 
-      const description = document.createElement("p");
-      description.textContent = repositories[i].description;
+// Burger Menu
 
-      const language = document.createElement("span");
-      language.textContent = "Language: " + repositories[i].language;
+// function setupBurgerMenu() {
+//   let burgerMenu = document.querySelector("#burger-menu");
+//   let overlay = document.querySelector("#menu");
 
-      project.appendChild(repoLink);
-      project.appendChild(description);
-      project.appendChild(language);
-      projectList.appendChild(project);
-    }
+//   burgerMenu.addEventListener("click", function () {
+//     this.classList.toggle("close");
+//     overlay.classList.toggle("overlay");
+//   });
+
+// }
+
+//Burger Menu Corr
+
+function setupBurgerMenu() {
+  let burgerMenu = document.querySelector("#burger-menu");
+  let overlay = document.querySelector("#menu");
+
+  function closeMenu() {
+    burgerMenu.classList.remove("close");
+    overlay.classList.remove("overlay");
   }
-});
+
+  // Toggle menu when burger icon is clicked
+  burgerMenu.addEventListener("click", function (event) {
+    event.stopPropagation();
+    this.classList.toggle("close");
+    overlay.classList.toggle("overlay");
+  });
+
+  // Close menu when clicking anywhere on the document
+  document.addEventListener("click", function (event) {
+    if (!overlay.contains(event.target) && !burgerMenu.contains(event.target)) {
+      closeMenu();
+    }
+  });
+
+  // Close menu when the window resizes
+  window.addEventListener("resize", closeMenu);
+}
+
+// Call the functions to execute the code
+addCopyrightToFooter();
+createSkillsList();
+setupLeaveMessageForm();
+fetchAndDisplayProjects();
+setupBurgerMenu();
